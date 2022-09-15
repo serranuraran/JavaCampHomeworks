d5ef516eaee5f28651f47579212d7c03aff08d49
serranuraran <serranuraran@gmail.com>
2022-09-15 23:23:22 +0300

    commit 3

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 int sayi = 12;
+	     sayi = 13;
+
+	     byte sayi2 = 13;
+	     sayi = 128;
+
+	     double sayi3 = 12.5;
+	     sayi3 = -129;
+
+	     char karakter = 'A';
+
+	     boolean dogruMu = false;
+
+	}
+
+}
