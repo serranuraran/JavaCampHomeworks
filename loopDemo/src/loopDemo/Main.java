d7d4514ad414b8eb67cddfca63162bc62bc9fdfb
serranuraran <serranuraran@gmail.com>
2022-09-16 02:04:15 +0300

    commit 8

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -7,10 +7,25 @@ public class Main {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("i = " + i);
 		}
-		System.out.println("Döngü Bitti");
+		System.out.println("For Döngüsü Bitti");
 
-		for (int i = 0; i < 10; i += 2) {    
+		for (int i = 0; i < 10; i += 2) {
 			System.out.println("i = " + i);
 		}
+		System.out.println("-------------");
+		// While Döngüsü
+		int i = 1;
+		while (i < 10) {
+			System.out.println("i = " + i);
+			i++;  //sayacýn artýþýný belirtmeliyiz, aksi halde sonsuz döngüye girer
+		}
+		
+		 //eðer ki tek sayýlarý yazdýrmak isteseydim
+		int j = 1;
+		while (j < 10) {
+			System.out.println("j = " + j);
+			j+=2;  //sayacýn artýþýný belirtmeliyiz, aksi halde sonsuz döngüye girer
+		}
+		System.out.println("While Döngüsü Bitti");
 	}
 }
