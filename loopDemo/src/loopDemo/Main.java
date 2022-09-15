292a2bb0f89b1622bac607e29b09ed936985621d
serranuraran <serranuraran@gmail.com>
2022-09-16 01:36:57 +0300

    commit 8

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,16 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// For Döngüsü
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i = " + i);
+		}
+		System.out.println("Döngü Bitti");
+
+		for (int i = 0; i < 10; i += 2) {    
+			System.out.println("i = " + i);
+		}
+	}
+}
