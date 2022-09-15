4e8f6fbba347e0ce32916a92abe686d5e58be0fa
serranuraran <serranuraran@gmail.com>
2022-09-15 19:27:22 +0300

    second commit

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//case sensitive
+		int ogrenciSayisi = 12;
+		String mesaj = "Öðrenci sayýsý: ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öðrenci sayým : "+ogrenciSayisi);
+		System.out.println("Öðrenci sayým : "+ogrenciSayisi);
+		System.out.println("Öðrenci sayým : "+ogrenciSayisi);
+	}
+
+}
