9fd0a4a0635daf0cd3e1bb7428f8a65e4bf4a157
serranuraran <serranuraran@gmail.com>
2022-09-23 02:08:54 +0300

    ornek

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,10 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(4, 8);
+		System.out.println(sonuc);
+	}
+}
