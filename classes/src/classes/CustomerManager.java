e617e51635ac772c6b14a5ae6bdc2cf5069d887d
serranuraran <serranuraran@gmail.com>
2022-09-23 02:00:47 +0300

    classlar

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class CustomerManager {
+
+	public void Add() {
+		System.out.println("Müþteri eklendi.");
+	}
+
+	public void Remove() {
+		System.out.println("Müþteri silindi.");
+	}
+
+	public void Update() {
+		System.out.println("Müþteri güncellendi.");
+	}
+}
---
+++ classes/src/classes/Main.java
@@ -0,0 +1,28 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        //classýn ismi 
+		CustomerManager customerManager = new CustomerManager();  //bir classýn çaðrýmý bu þekildedir, bunu dediðimizde bellekte o an customermanager türünde bir nesne üretmiþ oluyoruz 
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+
+		// Value types (int, double, float, boolean, char)
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi2 = sayi1;
+		sayi1 = 30;
+		System.out.println(sayi2);
+
+		// Reference types (arrays, classes)
+		int[] sayilar1 = new int[] { 1, 2, 3 };
+		int[] sayilar2 = new int[] { 10, 20, 30 };
+		sayilar2 = sayilar1;
+		sayilar1[0] = 1000;
+		System.out.println(sayilar2[0]);
+	}
+	
+}
