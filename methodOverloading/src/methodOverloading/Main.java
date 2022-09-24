282f949d7305708009b7917db6f07bc343cbe052
serranuraran <serranuraran@gmail.com>
2022-09-24 22:43:37 +0300

    overloading

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,10 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dort›slem dortIslem = new Dort›slem();
+        System.out.println(dortIslem.topla(2,3));
+        System.out.println(dortIslem.topla(2,3,4));
+    }
+}
