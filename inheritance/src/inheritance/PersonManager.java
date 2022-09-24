49b626ef35fcadf96c3fa7340cf4fddcd40383c6
serranuraran <serranuraran@gmail.com>
2022-09-24 22:56:23 +0300

    inheritance

---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class PersonManager {
+	public void List() {
+        System.out.println("Müþteri Listelendi");
+    }
+    public void Add() {
+        System.out.println("Müþteri Eklendi");
+    }
+}
