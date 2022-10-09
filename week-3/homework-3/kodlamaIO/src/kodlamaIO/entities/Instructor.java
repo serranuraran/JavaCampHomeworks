b57c862f520c1528798aa8db24716b62003c0d5f
serranuraran <serranuraran@gmail.com>
2022-10-09 15:58:12 +0300

    added
    commit

--- week-3/homework-3/kodlamaIO/src/kodlamaIO/entities/Instructor.java
+++ week-3/homework-3/kodlamaIO/src/kodlamaIO/entities/Instructor.java
@@ -1,5 +1,45 @@
 package kodlamaIO.entities;
 
 public class Instructor {
+	private int ýnstructorId;
+	private String ýnstructorName;
+	private String ýnstructorLastName;
+	
+	public Instructor() {
+		
+	}
+	
+	public Instructor(int instructorId, String instructorName, String instructorLastName) {
 
+		ýnstructorId = instructorId;
+		ýnstructorName = instructorName;
+		ýnstructorLastName = instructorLastName;
+	}
+	
+	public int getInstructorId() {
+		return ýnstructorId;
+	}
+
+	public void setInstructorId(int instructorId) {
+		ýnstructorId = instructorId;
+	}
+
+	public String getInstructorName() {
+		return ýnstructorName;
+	}
+
+	public void setInstructorName(String instructorName) {
+		ýnstructorName = instructorName;
+	}
+
+	public String getInstructorLastName() {
+		return ýnstructorLastName;
+	}
+
+	public void setInstructorLastName(String instructorLastName) {
+		ýnstructorLastName = instructorLastName;
+	}
+
+
+	
 }
